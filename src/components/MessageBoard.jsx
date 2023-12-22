fbcdc290396b45368f97973ca9875bf854e949ad
SamitaTT <m.samita.tt@gmail.com>
2023-12-22 14:47:04 +0700

    feat: completed

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,60 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [msgText, setMsgText] = useState("");
+  const [msg, setMsg] = useState([]);
+
+  const addMsg = (event) => {
+    event.preventDefault();
+    const newMsgList = [...msg];
+    newMsgList.push(msgText);
+    setMsg(newMsgList);
+  };
+
+  const deleteMsg = (msgIndex) => {
+    const newMsgList = [...msg];
+    newMsgList.splice(msgIndex, 1);
+    setMsg(newMsgList);
+  };
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            onChange={(event) => {
+              setMsgText(event.target.value);
+            }}
+            value={msgText}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={addMsg}>
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {msg.map((item, index) => {
+          return (
+            <div className="message">
+              <h1 className="message">
+                {item}
+                <button
+                  className="delete-button"
+                  onClick={() => {
+                    deleteMsg(index);
+                  }}
+                >
+                  x
+                </button>
+              </h1>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
